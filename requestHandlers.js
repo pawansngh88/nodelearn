23048d1d457845f3d2aeec03a8467fe701d9da42
Pawan Singh <pawansngh88@gmail.com>
2013-08-10 18:43:58 +0530

    Fixed bug

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(res) {
   console.log("Request handler 'start' was called.");
   /*function sleep(milliSeconds) {
     var startTime = new Date().getTime();
@@ -16,7 +16,7 @@ function start() {
       });
   return content;
 }
-function upload() {
+function upload(res) {
   console.log("Request handler 'upload' was called.");
   res.writeHead(200,{"Content-Type": "text/plain"});
   res.write("Hello upload");
--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 function route(handle, pathname, res) {
   console.log("About to route a request for " +pathname);
   if (typeof handle[pathname] === 'function') {
-    return handle[pathname]();
+    return handle[pathname](res);
   } else {
     console.log("No request handler found for " + pathname);
     res.writeHead(404,{"Content-Type": "text/plain"});
