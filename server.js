d1f9f02676e6c16c6d4d895f3d7d3992846b30db
Pawan Singh <pawansngh88@gmail.com>
2013-08-10 13:20:57 +0530

    Added request handler

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require('http');
 var url  = require('url');
-function start(route) {
+function start(route,handle) {
   http.createServer(function(req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log("Request for "+pathname+" received.");
-    route(pathname);
+    route(handle,pathname);
     res.writeHead(200,{"Content-Type": "text/plain"});
     res.write("Hello World");
     res.end();
