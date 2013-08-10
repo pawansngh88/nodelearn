6b21537c1629fe062f6a2c20d9837ce7dee27025
Pawan Singh <pawansngh88@gmail.com>
2013-08-10 12:34:42 +0530

    Added router

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var url  = require('url');
-function start() {
+function start(route) {
   http.createServer(function(req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log("Request for "+pathname+" received.");
+    route(pathname);
     res.writeHead(200,{"Content-Type": "text/plain"});
     res.write("Hello World");
     res.end();
