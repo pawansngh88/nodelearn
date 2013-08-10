991baf169610ce6ac665221804617e8dee41796e
Pawan Singh <pawansngh88@gmail.com>
2013-08-10 13:25:41 +0530

    Modifed request handlers

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ function start(route,handle) {
   http.createServer(function(req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log("Request for "+pathname+" received.");
-    route(handle,pathname);
     res.writeHead(200,{"Content-Type": "text/plain"});
-    res.write("Hello World");
+    var content = route(handle, pathname)
+    res.write(content);
+    //res.write("Hello World");
     res.end();
     }).listen(8888);
   console.log("Server started.");
