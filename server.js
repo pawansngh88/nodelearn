de4fd6f73195655673fd4737a5e8b74280b80faa
Pawan Singh <pawansngh88@gmail.com>
2013-08-10 12:29:39 +0530

    Modified parse url

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http');
 var url  = require('url');
 function start() {
   http.createServer(function(req, res) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for "+pathname+"received.");
+    var pathname = url.parse(req.url).pathname;
+    console.log("Request for "+pathname+" received.");
     res.writeHead(200,{"Content-Type": "text/plain"});
     res.write("Hello World");
     res.end();
