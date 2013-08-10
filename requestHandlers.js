75be3bda9836a8ed8f38e26db0011f6d1a8deeed
Pawan Singh <pawansngh88@gmail.com>
2013-08-10 18:30:13 +0530

    Improper blocking operation in request handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,18 @@
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
-  function sleep(milliSeconds) {
+  /*function sleep(milliSeconds) {
     var startTime = new Date().getTime();
     while (new Date().getTime() < startTime + milliSeconds);
   }
   sleep(10000);
-  return "Hello start";
+  return "Hello start";*/
+  var content = "empty";
+  exec("ls -lah", function (error, stdout, stderr) {
+      content = stdout;
+      });
+  return content;
 }
 function upload() {
   console.log("Request handler 'upload' was called.");
